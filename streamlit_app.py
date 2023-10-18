9d12fe273baa9e880c3b5514d975d8cc5b0225b0
Alexandra Andersson <106247155+solita-alexandra@users.noreply.github.com>
2023-10-18 14:34:02 +0200

    Added fruit names instead of index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,10 +10,17 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Smashed avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# Display the table on the page.
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
+
